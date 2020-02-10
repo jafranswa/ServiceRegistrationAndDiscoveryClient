64e3611bc2dd94d095291aa31c923f74253dfe04
jacob <jafranswa@gmail.com>
2020-02-09 22:45:54 -0600

    client tested and running

--- src/main/java/com/example/serviceregistrationanddiscoveryclient/ServiceRegistrationAndDiscoveryClientApplication.java
+++ src/main/java/com/example/serviceregistrationanddiscoveryclient/ServiceRegistrationAndDiscoveryClientApplication.java
@@ -30,6 +30,7 @@ class ServiceInstanceRestController {
 	@RequestMapping("/service-instances/{applicationName}")
 	public List<ServiceInstance> serviceInstancesByApplicationName(
 			@PathVariable String applicationName) {
+
 		return this.discoveryClient.getInstances(applicationName);
 	}
 }
